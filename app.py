88d283af87d6ecbb3a93ee0928908483def0b685
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-08-09 09:37:08 -0400

    Pass Yards Bar Graph

--- app.py
+++ app.py
@@ -1,46 +1,59 @@
-import dash
-from dash import Dash
-from dash import dcc
-from dash import html
-from dash.dependencies import Input, Output
 import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 import plotly.express as px
 
-# Load your NFL player statistics data into a Pandas DataFrame
+# Load your CSV data
 df = pd.read_csv('./data/nfl_offensive_stats.csv')
 
 def get_player_subset_of_dataset(player: str = "Aaron Rodgers", 
-                                 columns_to_isolate: list = ["game_id", "player", "pass_yds"], ):
+                                columns_to_isolate: list = ["game_id", "player", "pass_yds"]):
+    # Filter data for a specific player
     PLAYER_NAME_MATCH = (df["player"] == player)
-    print(f"Player is `{player}`.")
     return df[columns_to_isolate][PLAYER_NAME_MATCH]
 
+# Initialize the Dash app
 app = dash.Dash(__name__)
 
 # Define the layout of the dashboard
-
 app.layout = html.Div([
     html.H1("NFL Player Statistics Dashboard"),
-    html.P("Choose a player to view their statistics from 2019- 2022."),
+    html.P("Choose a player to view their statistics from 2019-2022."),
+    
+    # Dropdown to select a player
     dcc.Dropdown(
         id='player-dropdown',
-        options=[{'label': player, 'value': player_id} for player, player_id in zip(df['player'], df['player_id'])],
-        value=df['player_id'][0],
+        options=[{'label': player, 'value': player} for player in df['player'].unique()],
+        value="Aaron Rodgers",  # Default selected player
         multi=False,
         style={'width': '50%'}
     ),
-    dcc.Graph(
-        id='player-stats'),
+    
+    # Graph to display player statistics
+    dcc.Graph(id='player-stats'),
 ])
 
+# Callback to update the graph based on selected player
 @app.callback(
-    Output('player-stats', 'figure'),
-    [Input('player-dropdown', 'value')]
+    dash.dependencies.Output('player-stats', 'figure'),
+    [dash.dependencies.Input('player-dropdown', 'value')]
 )
-def update_graph(selected_player_id):
-    selected_player_data = df[df['player_id'] == selected_player_id]
-    fig = px.bar(selected_player_data, x='stat_category', y='stat_value', title="Player Statistics")
+def update_player_stats(selected_player):
+    # Get subset of data for the selected player
+    player_data = get_player_subset_of_dataset(selected_player, ["game_id", "player", "pass_yds"])
+
+    # Create a bar graph using Plotly Express
+    fig = px.bar(player_data, x='game_id', y='pass_yds', title=f"{selected_player} Pass Yards by Game ID")
+
+    # Customize bar colors based on pass yards value
+    fig.update_traces(marker=dict(color=['green' if y > 250 else 'red' for y in player_data['pass_yds']]))
+
+    fig.update_yaxes(title_text="Pass Yards")
+    fig.update_xaxes(title_text="Opponent")
+
     return fig
 
+# Run the app if the script is executed
 if __name__ == '__main__':
     app.run_server(debug=True)
