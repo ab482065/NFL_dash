71fb0a27978d6fdaff3a33bb0798591f679fa37f
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-10-08 10:51:04 -0400

    Working on refactoring to postgresql

---
+++ connection.py
@@ -0,0 +1,53 @@
+from dotenv import load_dotenv
+import psycopg2
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+class DB:
+    def __init__(self, db_url = os.environ['DATABASE_URL']):
+        print({'db_url': db_url})
+        self.db_url = db_url
+        self.connection = None
+        self.cursor = None
+
+    def connect(self):
+        try:
+            self.connection = psycopg2.connect(self.db_url)
+            self.cursor = self.connection.cursor()
+            return True
+        except Exception as e:
+            print(f"Error connecting to the database: {e}")
+            return False
+
+    def close(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.connection:
+            self.connection.close()
+            
+    
+    def getConnection():
+        conn = DB()
+        conn.connect()
+        return conn
+
+# Example usage:
+if __name__ == "__main__":
+    # Replace this with your PostgreSQL connection URL
+    db_url =  os.environ['DATABASE_URL']
+
+    # Create an instance of the DatabaseWrapper class
+    db_wrapper = DB(db_url)
+
+    # Attempt to connect to the database
+    if db_wrapper.connect():
+        # Now you can access the connection and cursor as public members
+        print("Connected to the database!")
+        # Example query
+        db_wrapper.cursor.execute("SELECT * FROM users")
+        result = db_wrapper.cursor.fetchall()
+        print(result)
+        # Close the cursor and connection when done
+        db_wrapper.close()
